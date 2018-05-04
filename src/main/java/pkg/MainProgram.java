63eba6691e9375c18ea5bb0a435bf4f97d934e6c
Ovidio Calvet <calveto@gmail.com>
2018-05-04 19:03:40 -0400

    add settings to run project

--- src/main/java/pkg/MainProgram.java
+++ src/main/java/pkg/MainProgram.java
@@ -2,6 +2,6 @@ package pkg;
 
 public class MainProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello There");
     }
 }
